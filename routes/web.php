64aef0dff74cfef4448d09c00d0f257e01900133
javihen <62959381+javihen@users.noreply.github.com>
2025-11-04 21:42:46 -0400

    feat - modulo curso - creacion del controlador y la ruta que envia al index del curso

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/curso', function () {
+/* Route::get('/curso', function () {
     return view('curso.index');
-});
+}); */
+
+Route::get('/curso', [CursoController::class, 'index']);
