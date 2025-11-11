86e646703a170d18e56b78d58d987cb0fbf18db6
javihen <javi_hen@hotmail.com>
2025-11-11 12:15:37 -0400

    feat - MODULO ESTUDIANTE - diseño del panel estudiante para el listado

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -37,29 +37,70 @@
                         <td>Opciones</td>
                     </tr>
                     <tr class="border-b border-[#64748B] text-sm text-center ">
-                        <td class="py-2">Nro.</td>
-                        <td>Codigo</td>
-                        <td>Ap. Paterno</td>
-                        <td>Ap. Materno</td>
-                        <td>Nombres</td>
-                        <td>Curso</td>
-                        <td>Estado</td>
-                        <td>Opciones</td>
+                        <td class="py-3">1.</td>
+                        <td>E5A112</td>
+                        <td>ACUCHIRI</td>
+                        <td>CALLISAYA</td>
+                        <td>ARNOLD CESAR</td>
+                        <td>C4A1</td>
+                        <td>
+                            <a href="#"
+                                class="px-2 border border-green-500 bg-white text-green-500 rounded-sm hover:text-white hover:bg-green-500">Efectivo</a>
+                        </td>
+                        <td>
+                            <a href="#"
+                                class="py-2 px-3 border border-red-500 bg-white my-2 rounded-sm text-red-500 hover:bg-red-500 hover:text-white"><i
+                                    class='bx  bx-trash '></i></a>
+                            <a href="#"
+                                class="ml-1 p-2 border border-[#3B82F6] bg-white text-[#3B82F6] hover:bg-[#3B82F6] hover:text-white rounded-sm"><i
+                                    class='bx  bx-edit-alt '></i>
+                                Editar</a>
+                        </td>
                     </tr>
-                    <tr class="border-b border-[#64748B] text-sm text-center ">
-                        <td class="py-2">Nro.</td>
-                        <td>Codigo</td>
-                        <td>Ap. Paterno</td>
-                        <td>Ap. Materno</td>
-                        <td>Nombres</td>
-                        <td>Curso</td>
-                        <td>Estado</td>
-                        <td>Opciones</td>
-                    </tr>
+
                 </table>
             </div>
-            <div class="bg-white w-1/3  border border-slate-300">
-                estadistica por curso
+            <div class=" w-1/3  ">
+                <div class=" flex gap-2">
+                    <a href="#"
+                        class="bg-[#3B82F6] px-4 py-2 text-white w-2/3 flex-auto text-center border-2 hover:border-blue-800 rounded-md "><i
+                            class='bx  bx-plus'></i>
+                        Nuevo
+                        estudiante
+                    </a>
+                    <a href="#"
+                        class="rounded-md w-1/3 flex-auto bg-slate-500 text-white text-center py-2 border-2 border-slate-500 hover:border-white"><i
+                            class='bx bx-download'></i> Subir </a>
+                </div>
+                <div class="border border-slate-300 bg-white">
+                    <p class="text-center p-2">Estadistica</p>
+                    <table class="w-full text-center">
+                        <tr class="bg-slate-500 text-white ">
+                            <td>Curso</td>
+                            <td>M</td>
+                            <td>F</td>
+                            <td>Total</td>
+                        </tr>
+                        <tr class="border-b border-slate-300">
+                            <td>1o PRIMARIA "A"</td>
+                            <td>32</td>
+                            <td>8</td>
+                            <td>40</td>
+                        </tr>
+                        <tr class="border-b border-slate-300">
+                            <td>1o PRIMARIA "A"</td>
+                            <td>32</td>
+                            <td>8</td>
+                            <td>40</td>
+                        </tr>
+                        <tr class="border-b border-slate-300">
+                            <td>1o PRIMARIA "A"</td>
+                            <td>32</td>
+                            <td>8</td>
+                            <td>40</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
 
