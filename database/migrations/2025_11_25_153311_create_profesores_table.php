55ac3473a0363ba7761e63f760b1e8897c78156a
javihen <javi_hen@hotmail.com>
2025-11-25 11:58:01 -0400

    feat - MODULO PROFESOR - se creó el controlador y modelo para la gestión de profesores, además de la migración para la tabla de profesores

---
+++ database/migrations/2025_11_25_153311_create_profesores_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profesores', function (Blueprint $table) {
+            $table->bigIncrements('id_profesor');
+            $table->string('ci');
+            $table->string('rda');
+            $table->string('nombres');
+            $table->string('appaterno');
+            $table->string('apmaterno');
+            $table->char('genero',1);
+            $table->date('fechaNac');
+            $table->string('fuenteFinan');
+            $table->string('nivelFormacion');
+            $table->char('estado',1)->default('A');
+            $table->string('observacion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profesores');
+    }
+};
